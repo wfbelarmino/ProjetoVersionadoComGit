ced3458efe1c78b7b217d7b1fdf52f0ee01059d9
WagnerSouza <wsantsou@everis.com>
2021-10-20 14:44:41 -0300

    Adicionado comando para imprimir a data de hoje.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ProjetoVersionadoComGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+        
         }
     }
 }
